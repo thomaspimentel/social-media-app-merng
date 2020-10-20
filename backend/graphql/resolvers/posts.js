8e582e652e7a9501fbf5db7a2eb2dff793623f0b
Thomas Pimentel <Thomas@webscaping.ca>
2020-10-20 12:12:46 -0400

    Added createPost with jwt auth

--- backend/graphql/resolvers/posts.js
+++ backend/graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/check-auth');
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({createdAt: -1});
                 return posts;
             } catch (err) {
                 throw new Error(err);
@@ -24,7 +25,18 @@ module.exports = {
         }
     }, Mutation: {
         async createPost(_, { body }, context) {
-            
+            const user = checkAuth(context);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date()
+            });
+
+            const post = await newPost.save();
+
+            return post;
         }
     }
 }
